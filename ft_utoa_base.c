6aa64ace1e5768c60c86d033ee70156aa81938a2
tavio <tavio92.tm@gmail.com>
2020-05-07 15:40:54 +0200

    added ft_utoa_base modified:   ft_itoa_base.c now return NULL if base if invalid

--- ft_utoa_base.c
+++ ft_utoa_base.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa_base.c                                     :+:      :+:    :+:   */
+/*   ft_utoa_base.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ocmarout <ocmarout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/04 11:20:21 by ocmarout          #+#    #+#             */
-/*   Updated: 2020/05/07 15:20:34 by ocmarout         ###   ########.fr       */
+/*   Updated: 2020/05/07 15:20:15 by ocmarout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,13 @@ static char	*ft_write_str(int len, unsigned int mod, unsigned int nb,
 		const char *base)
 {
 	int		i;
-	int		sign;
 	int		len_base;
 	char	*str;
 
 	i = 0;
-	sign = len < 0 ? -1 : 1;
-	len = len < 0 ? len * -1 : len;
 	len_base = ft_strlen(base);
 	if (!(str = malloc(sizeof(char) * len)))
 		return (0);
-	if (sign == -1)
-		str[i++] = '-';
 	while (mod > 0)
 	{
 		str[i++] = base[nb / mod];
@@ -38,26 +33,23 @@ static char	*ft_write_str(int len, unsigned int mod, unsigned int nb,
 	return (str);
 }
 
-char		*ft_itoa_base(int n, const char *base)
+char		*ft_utoa_base(unsigned int n, const char *base)
 {
 	int				i;
 	int				len;
 	int				len_base;
 	long			mod;
-	unsigned int	nb;
 
 	len_base = ft_check_base(base);
 	if (len_base < 2)
 		return (0);
 	i = 0;
 	mod = 1;
-	len = n < 0 ? 3 : 2;
-	nb = n < 0 ? (unsigned)n * -1 : (unsigned)n;
-	while (mod * len_base <= nb)
+	len = 2;
+	while (mod * len_base <= n)
 	{
 		len++;
 		mod *= len_base;
 	}
-	len = n < 0 ? len * -1 : len;
-	return (ft_write_str(len, mod, nb, base));
+	return (ft_write_str(len, mod, n, base));
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ocmarout <ocmarout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 15:10:47 by ocmarout          #+#    #+#             */
-/*   Updated: 2020/05/07 14:38:27 by ocmarout         ###   ########.fr       */
+/*   Updated: 2020/05/07 15:36:57 by ocmarout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ typedef struct	s_list
 	struct s_list	*next;
 }				t_list;
 
-size_t			ft_strlen(const char *s);
-size_t			ft_strlcpy(char *dest, const char *src, size_t size);
-size_t			ft_strlcat(char *dest, const char *src, size_t size);
+size_t			ft_strlen(char const *s);
+size_t			ft_strlcpy(char *dest, char const *src, size_t size);
+size_t			ft_strlcat(char *dest, char const *src, size_t size);
 int				ft_isdigit(int c);
 int				ft_isalpha(int c);
 int				ft_isalnum(int c);
@@ -34,29 +34,29 @@ int				ft_isprint(int c);
 int				ft_isspace(int c);
 int				ft_toupper(int c);
 int				ft_tolower(int c);
-int				ft_check_base(const char *base);
-int				ft_strncmp(const char *s1, const char *s2, size_t n);
-int				ft_atoi(const char *str);
-int				ft_memcmp(const void *s1, const void *s2, size_t n);
-void			ft_putstr_fd(char *s, int fd);
-void			ft_putendl_fd(char *s, int fd);
+int				ft_check_base(char const *base);
+int				ft_strncmp(char const *s1, char const *s2, size_t n);
+int				ft_atoi(char const *str);
+int				ft_memcmp(void const *s1, void const *s2, size_t n);
+void			ft_putstr_fd(char const *s, int fd);
+void			ft_putendl_fd(char const *s, int fd);
 void			ft_putnbr_fd(int n, int fd);
 void			ft_bzero(void *s, size_t n);
 void			*ft_calloc(size_t count, size_t size);
 void			*ft_memset(void *b, int c, size_t len);
-void			*ft_memcpy(void *dst, const void *src, size_t n);
-void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
-void			*ft_memmove(void *dst, const void *src, size_t len);
-void			*ft_memchr(const void *s, int c, size_t n);
-char			*ft_strcpy(char *str, const char *src);
-char			*ft_strchr(const char *s, int c);
-char			*ft_strrchr(const char *s, int c);
-char			*ft_strnstr(const char *h, const char *n, size_t l);
-char			*ft_strdup(const char *src);
+void			*ft_memcpy(void *dst, void const *src, size_t n);
+void			*ft_memccpy(void *dst, void const *src, int c, size_t n);
+void			*ft_memmove(void *dst, void const *src, size_t len);
+void			*ft_memchr(void const *s, int c, size_t n);
+char			*ft_strcpy(char *str, char const *src);
+char			*ft_strchr(char const *s, int c);
+char			*ft_strrchr(char const *s, int c);
+char			*ft_strnstr(char const *h, char const *n, size_t l);
+char			*ft_strdup(char const *src);
 char			*ft_itoa(int n);
-char			*ft_itoa_base(int n, const char *base);
+char			*ft_itoa_base(int n, char const *base);
 char			*ft_utoa(unsigned int n);
-char			*ft_utoa_base(unsigned int n, const char *base);
+char			*ft_utoa_base(unsigned int n, char const *base);
 char			*ft_substr(char const *s, unsigned int start, size_t l);
 char			*ft_strjoin(char const *s1, char const *s2);
 char			*ft_strtrim(char const *s1, char const *set);
