4c663e8f084cd933f0ed84250379d74cf9f25b31
tavio <ocmarout@student.42.fr>
2021-08-19 11:48:55 +0200

    add a fix to some of the doubly linked lists functions

--- lists/ft_2lstlast.c
+++ lists/ft_2lstlast.c
@@ -1,25 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_2lstadd_front.c                                 :+:      :+:    :+:   */
+/*   ft_2lstlast.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ocmarout <ocmarout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/18 13:25:12 by ocmarout          #+#    #+#             */
-/*   Updated: 2021/08/18 16:57:57 by ocmarout         ###   ########.fr       */
+/*   Created: 2021/08/18 17:11:24 by ocmarout          #+#    #+#             */
+/*   Updated: 2021/08/18 17:14:10 by ocmarout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_2lstadd_front(t_2list **alst, t_2list *new)
+t_2list	*ft_2lstlast(t_2list *lst)
 {
-	ft_printf("1\n");
-	new->next = *alst;
-	ft_printf("2\n");
-	if (*alst)
-		(*alst)->prev = new;
-	ft_printf("3\n");
-	*alst = new;
-	ft_printf("4\n");
+	if (!lst)
+		return (0);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
 }
